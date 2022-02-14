ce0dfc2c03b11ad148d09b1c885a5813a95a686a
Yatendra1915 <97654605+Yatendra1915@users.noreply.github.com>
2022-02-14 23:13:21 +0530

    Update Copy List with Random Pointer.java

--- DAY-7/Copy List with Random Pointer.java
+++ DAY-7/Copy List with Random Pointer.java
@@ -24,3 +24,43 @@ class Solution {
         return hs.get(head);
     }
 }
+
+//Efficient
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head==null)
+            return null;
+        
+        Node curr=head;
+        
+        while(curr!=null){
+            Node temp =curr.next;
+            curr.next=new Node(curr.val);
+            curr.next.next=temp;
+            curr=temp;
+        }
+        
+        curr=head;
+        
+        while(curr!=null ){
+            
+            if(curr.next!=null)
+             curr.next.random=(curr.random!=null)?curr.random.next:null;
+            curr=curr.next.next;
+        }
+        
+        curr=head.next;
+        Node ans=curr;
+        Node original = head;
+        while(curr!=null && original!=null){
+            original.next = (original.next!=null)? original.next.next : original.next;
+            curr.next=(curr.next!=null)?curr.next.next:null;
+            
+            
+            original = original.next;
+            curr=curr.next;
+        }
+        return ans;
+        
+    }
+}
