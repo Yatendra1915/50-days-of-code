f85f4bc5837a84f7e69ff36b61ca877cb3bd7b6a
Yatendra1915 <97654605+Yatendra1915@users.noreply.github.com>
2022-03-02 00:07:34 +0530

    Create CombinationSum2.java

---
+++ DAY-9/CombinationSum2.java
@@ -0,0 +1,42 @@
+/*https://leetcode.com/problems/combination-sum-ii/*/
+class Solution {
+    
+    Set<List<Integer>> set=new HashSet<>();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+        List<List<Integer>> list =new ArrayList<>();
+       
+        Arrays.sort(candidates);
+        helper(candidates,target,0,new ArrayList<>());
+        
+        for(List<Integer> ls:set){
+            list.add(ls);
+        }
+        
+        return list;
+    }
+    
+    public void helper(int[] candidates,int target,int i,List<Integer> temp){
+        
+        if(target==0){
+            set.add(new ArrayList(temp));
+            return;
+        }
+        
+        if(i>=candidates.length)
+            return ;
+    
+        
+        if(target-candidates[i]>=0){
+            temp.add(candidates[i]);
+            helper(candidates,target-candidates[i],i+1,temp);
+            temp.remove(temp.size()-1);
+        }
+        
+        while(i+1 < candidates.length && candidates[i]==candidates[i+1]) {
+           i++;     
+       }
+        
+        helper(candidates,target,i+1,temp);
+    }
+}
